ae43665632c50d365ddcdda51c85322ffbced98f
Joel Brubaker <joel@tipsyelves.com>
2017-04-20 19:38:46 -0700

    another commit

--- src/main.js
+++ src/main.js
@@ -3,8 +3,21 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+var config = {
+  apiKey: "AIzaSyBYbipQgYDFMgO8uGL67drC9t5kUM4-0iw",
+  authDomain: "vue-meetup-9ce1a.firebaseapp.com",
+  databaseURL: "https://vue-meetup-9ce1a.firebaseio.com",
+  projectId: "vue-meetup-9ce1a",
+  storageBucket: "vue-meetup-9ce1a.appspot.com",
+  messagingSenderId: "463478566258"
+}
+
+firebase.initializeApp(config)
+
 
 /* eslint-disable no-new */
 new Vue({
